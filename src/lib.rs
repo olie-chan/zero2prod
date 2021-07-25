4bbfeda23d0c18f2177924f229cdd51a1f2d6821
Olie Chan <olie.chan@infinityworks.com>
2021-07-25 20:53:02 +0100

    use extractor for form data

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 use std::net::TcpListener;
+
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-async fn subscribe() -> HttpResponse {
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
