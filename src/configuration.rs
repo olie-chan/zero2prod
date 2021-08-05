6c61abff0cadbcf5e7310da6003bf41073faf0ef
Olie Chan <olie.chan.141@gmail.com>
2021-08-05 23:02:10 +0100

    add configuration file

---
+++ src/configuration.rs
@@ -0,0 +1,24 @@
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let mut settings = config::Config::default();
+
+    settings.merge(config::File::with_name("configuration"))?;
+
+    settings.try_into()
+}
