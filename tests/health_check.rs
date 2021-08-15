ef29300540130415b5819258a13897380c7a63a0
Olie Chan <olie.chan.141@gmail.com>
2021-08-15 15:44:34 +0100

    refactor tracing

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -10,8 +10,16 @@ use zero2prod::{
 };
 
 static TRACING: Lazy<()> = Lazy::new(|| {
-    let subscriber = get_subscriber("test".into(), "debug".into());
-    init_subscriber((subscriber));
+    let subscriber_name = "test".to_string();
+    let default_filter_level = "info".to_string();
+
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
 });
 
 pub struct TestApp {
