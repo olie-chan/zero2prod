4b5ab3b8e4d28e84a0dc59e6de256770a1e46f2e
Olie Chan <olie.chan.141@gmail.com>
2021-09-11 13:48:06 +0100

    add unit tests

--- src/domain.rs
+++ src/domain.rs
@@ -45,4 +45,30 @@ mod tests {
         let name = "a".repeat(257);
         assert_err!(SubscriberName::parse(name));
     }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            let name = name.to_string();
+            assert_err!(SubscriberName::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "Ursula Le Guin".to_string();
+        assert_ok!(SubscriberName::parse(name));
+    }
 }
