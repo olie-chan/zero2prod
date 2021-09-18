039783e72a328f9c9ade156bceb4fa1f775ff904
Olie Chan <olie.chan.141@gmail.com>
2021-09-18 23:20:02 +0100

    refactor subscriber parsing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,6 +11,12 @@ pub struct FormData {
     name: String,
 }
 
+pub fn parse_subscriber(form: FormData) -> Result<NewSubscriber, String> {
+    let name = SubscriberName::parse(form.name)?;
+    let email = SubscriberEmail::parse(form.email)?;
+    Ok(NewSubscriber { name, email })
+}
+
 #[tracing::instrument(
   name = "Adding a new subscriber",
   skip(form, pool),
@@ -20,15 +26,10 @@ pub struct FormData {
   )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
+    let new_subscriber = match parse_subscriber(form.0) {
+        Ok(subscriber) => subscriber,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    let new_subscriber = NewSubscriber { email, name };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
